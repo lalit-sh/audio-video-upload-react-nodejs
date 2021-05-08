6fef3a70eb0e10c7d959c5568356ce4221888950
Lalit Sharma <dev.lalit.sh@gmail.com>
2021-05-08 15:08:18 +0530

    make it better

--- src/db/index.js
+++ src/db/index.js
@@ -3,21 +3,18 @@ mongoose.Promise = require("bluebird");
 
 let dbName = process.env.DB_NAME;
 const dbAddress = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
-// console.log("process.env", process.env)
-if(!dbName || !dbAddress || !dbPort){
-    throw new Error("Mongo error unable to configuredatabase");
-}
+// const dbPort = process.env.DB_PORT;
 
 let options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    user:process.env.DB_USER,
-    pass: process.env.DB_PASS,
-    dbName: dbName
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // user:process.env.DB_USER,
+    // pass: process.env.DB_PASS,
+    // dbName: dbName
+    useNewUrlParser: true, useUnifiedTopology: true
 };
 
-mongoose.connect(`mongodb://${dbAddress}:${dbPort}`, options).catch(err => {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${dbAddress}/${dbName}?retryWrites=true&w=majority`, options).catch(err => {
     if (err.message.indexOf("ECONNREFUSED") !== -1) {
         console.error("Error: The server was not able to reach MongoDB. Maybe it's not running?");
         process.exit(1);
@@ -25,3 +22,5 @@ mongoose.connect(`mongodb://${dbAddress}:${dbPort}`, options).catch(err => {
         throw err;
     }
 });
+
+
