16ef5477855b59df600f7e32ff5a087a9b294a44
Lalit Sharma <dev.lalit.sh@gmail.com>
2021-05-23 17:02:57 +0530

    latest changes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,10 +5,6 @@ module.exports = app => {
     let controller = new Controller();
     
     app.route(`${process.env.API_BASE}getUserDetails`).get((req, res) => controller.getUserDetails(req, res))
-    // app.route(`/login`).post(passport.authenticate('local', {
-    //     failureRedirect: '/login',
-    //     failureFlash: false
-    // }), (req, res) => controller.login(req, res))
     app.route(`/login`).post((req, res) => controller.login(req, res))
     app.route("/signup").post((req, res) => controller.signup(req, res));
     app.route("/logout").get((req, res) => controller.logout(req, res));
